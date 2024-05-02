84aae5d24a1ddcbe0a530b4e0b10ad254870ac00
VladiusVostokus <kovalenkov222@gmail.com>
2024-05-02 17:39:23 +0300

    Draw black rectangle as example

--- ui/window.go
+++ ui/window.go
@@ -121,9 +121,13 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 			if e.Button == 3 && e.Direction == 1 {
 				x := e.X
 				y := e.Y
-				fmt.Println(x,y)
+				x1 := int(x)
+				y1 := int(y)
+				x2 := x1 + 400
+				y2 := y1 + 400
+				fmt.Println(x1, y1, x2, y2)
 			}
-			// TODO: Реалізувати реакцію на натискання кнопки миші.
+			// TODO: Реалізуваdти реакцію на натискання кнопки миші.
 		}
 
 	case paint.Event:
@@ -140,6 +144,15 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src) // Фон.
+
+	x1 := 0
+	y1 := 0
+	x2 := x1 + 400
+	y2 := y1 + 400
+	fmt.Println(x1, y1, x2, y2)
+	i := image.Rect(x1, y1, x2, y2)
+
+	pw.w.Fill(i.Bounds(), color.Black, draw.Src)
 
 	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
 
