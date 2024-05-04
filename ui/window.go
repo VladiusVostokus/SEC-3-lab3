8b2f1c9e40bd969d64271f01136769bc48064274
VladiusVostokus <kovalenkov222@gmail.com>
2024-05-04 18:22:41 +0300

    Drawinf cross by pressing right mouse

--- ui/window.go
+++ ui/window.go
@@ -1,7 +1,6 @@
 package ui
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"log"
@@ -27,19 +26,16 @@ type Visualizer struct {
 	done chan struct{}
 
 	sz  size.Event
-	pos image.Rectangle
+	pos image.Point
 }
 
-const (
-	defaultX = 200
-	defaultY = 300
-)
+var blue = color.RGBA{0, 0, 255, 0}
 
 func (pw *Visualizer) Main() {
 	pw.tx = make(chan screen.Texture)
 	pw.done = make(chan struct{})
-	pw.pos.Max.X = 200
-	pw.pos.Max.Y = 200
+	pw.pos.X = 200
+	pw.pos.Y = 300
 	driver.Main(pw.run)
 }
 
@@ -124,14 +120,9 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 	case mouse.Event:
 		if t == nil {
 			if e.Button == 3 && e.Direction == 1 {
-				x := e.X
-				y := e.Y
-				x1 := int(x)
-				y1 := int(y)
-				//x2 := x1 + 400
-				//y2 := y1 + 400
-				fmt.Println(x1, y1)
-				//pw.w.Send(paint.Event{})
+				pw.pos.X = int(e.X)
+				pw.pos.Y = int(e.Y)
+				pw.w.Send(paint.Event{})
 			}
 		}
 
@@ -147,32 +138,29 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 	}
 }
 
-func drawCross(x, y int) {
+func (pw *Visualizer) drawCross() {
+	x1 := pw.pos.X
+	y1 := pw.pos.Y
+	x2 := x1 + 400
+	y2 := y1 + 200
+
+	i := image.Rect(x1, y1, x2, y2)
+	pw.w.Fill(i.Bounds(), blue, draw.Src)
+
+	x1 = x1 + 100
+	y1 = y1 - 100
+	x2 = x1 + 200
+	y2 = y1 + 400
+	i = image.Rect(x1, y1, x2, y2)
+	pw.w.Fill(i.Bounds(), blue, draw.Src)
 }
 
 func (pw *Visualizer) drawDefaultUI() {
 
 	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src) // Фон.
 
-	x1 := defaultX
-	y1 := defaultY
-	x2 := x1 + 400
-	y2 := y1 + 200
+	pw.drawCross()
 
-	i := image.Rect(x1, y1, x2, y2)
-	blue := color.RGBA{0, 0, 255, 0}
-	pw.w.Fill(i.Bounds(), blue, draw.Src)
-
-	x1 = 300
-	y1 = 200
-	x2 = x1 + 200
-	y2 = y1 + 400
-	i = image.Rect(x1, y1, x2, y2)
-	pw.w.Fill(i.Bounds(), blue, draw.Src)
-
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
-
-	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
 		pw.w.Fill(br, color.White, draw.Src)
 	}
