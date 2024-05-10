0fb3b674dd83b7100b4eedb31eb1b0c9d92bcbfe
VladiusVostokus <kovalenkov222@gmail.com>
2024-05-10 20:51:35 +0300

    Check len and command errors, write parameters into console

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -22,23 +22,46 @@ func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 		words := strings.Split(commandLine, " ")
 		//fmt.Println(words)
 		command := words[0]
+		wordsLen := len(words)
 		switch command {
 		case "white":
 			fmt.Println("white background")
+			if wordsLen != 1 {
+				return nil, fmt.Errorf("Too many parameters for white command")
+			}
+			fmt.Println("white background")
+
 		case "green":
+			if wordsLen != 1 {
+				return nil, fmt.Errorf("Too many parameters for green command")
+			}
 			fmt.Println("green background")
+
 		case "update":
+			if wordsLen != 1 {
+				return nil, fmt.Errorf("Too many parameters for updare command")
+			}
 			fmt.Println("update texture")
+
 		case "figure":
-			fmt.Println("new fugire with coords")
+			if wordsLen != 3 {
+				return nil, fmt.Errorf("Must be 2 parametrs for this command")
+			}
+			fmt.Println("new figure with coords", words[1], words[2])
 		case "move":
-			fmt.Println("move figure to")
+			if wordsLen != 3 {
+				return nil, fmt.Errorf("Must be 2 parametrs for this command")
+			}
+			fmt.Println("move figure to", words[1], words[2])
 		case "bgrect":
-			fmt.Println("draw background with size")
+			if wordsLen != 5 {
+				return nil, fmt.Errorf("Must be 4 parametrs for this command")
+			}
+			fmt.Println("draw background with size", words[1], words[2], words[3], words[4])
 		case "reset":
 			fmt.Println("reset texture state")
 		default:
-			fmt.Println("Wrong command")
+			return nil, fmt.Errorf("Wrond command")
 		}
 
 		//op := parse(commandLine) // parse the line to get Operation
