ec07b874455d2232f352b2dc5391cc921bce80a6
VladiusVostokus <kovalenkov222@gmail.com>
2024-05-15 12:21:26 +0300

    initial realization of white and green commands

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -22,64 +22,69 @@ func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 		commands := strings.Split(commandLine, " ")
 		//fmt.Println(words)
 
-		err := p.parse(commands)
+		op, err := p.parse(commands)
 		if err != nil {
 			return nil, err
 		}
 
 		//op := parse(commandLine) // parse the line to get Operation
-		//res = append(res, op)
+		res = append(res, op)
 	}
 
 	// TODO: Реалізувати парсинг команд.
-	res = append(res, painter.OperationFunc(painter.WhiteFill))
+	//res = append(res, painter.OperationFunc(painter.WhiteFill))
 	res = append(res, painter.UpdateOp)
 
 	return res, nil
 }
 
-func (p *Parser) parse(commands []string) error {
+func (p *Parser) parse(commands []string) (painter.Operation, error) {
 	command := commands[0]
 	wordsLen := len(commands)
+	var op painter.Operation
 	switch command {
 	case "white":
-		fmt.Println("white background")
 		if wordsLen != 1 {
-			return fmt.Errorf("too many parameters for white command")
+			return nil, fmt.Errorf("too many parameters for white command")
 		}
-		fmt.Println("white background")
+		op = painter.OperationFunc(painter.WhiteFill)
 
 	case "green":
 		if wordsLen != 1 {
-			return fmt.Errorf("too many parameters for green command")
+			return nil, fmt.Errorf("too many parameters for green command")
 		}
-		fmt.Println("green background")
+		op = painter.OperationFunc(painter.GreenFill)
 
 	case "update":
 		if wordsLen != 1 {
-			return fmt.Errorf("too many parameters for updare command")
+			return nil, fmt.Errorf("too many parameters for updare command")
 		}
 		fmt.Println("update texture")
 
 	case "figure":
 		if wordsLen != 3 {
-			return fmt.Errorf("must be 2 parametrs for this command")
+			return nil, fmt.Errorf("must be 2 parametrs for this command")
 		}
 		fmt.Println("new figure with coords", commands[1], commands[2])
+
 	case "move":
 		if wordsLen != 3 {
-			return fmt.Errorf("must be 2 parametrs for this command")
+
+			return nil, fmt.Errorf("must be 2 parametrs for this command")
 		}
 		fmt.Println("move figure to", commands[1], commands[2])
+
 	case "bgrect":
 		if wordsLen != 5 {
-			return fmt.Errorf("must be 4 parametrs for this command")
+			return nil, fmt.Errorf("must be 4 parametrs for this command")
 		}
 		fmt.Println("draw background with size", commands[1], commands[2], commands[3], commands[4])
+
 	case "reset":
 		fmt.Println("reset texture state")
+
 	default:
-		return fmt.Errorf("wrond command")
+		return nil, fmt.Errorf("wrond command")
 	}
-	return nil
+	return op, nil
 }
